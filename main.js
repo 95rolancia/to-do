94539dd028ba285cc76a41cd31307e682be71270
jangjunhyeok <95jjangjun@gmail.com>
2021-02-03 18:15:47 +0900

    refactoring

--- main.js
+++ main.js
@@ -1,39 +1,60 @@
-const content = document.querySelector(".content");
+const items = document.querySelector(".items");
 const addbarBtn = document.querySelector(".addbar__btn");
 const addbarInput = document.querySelector(".addbar__input");
+let id = 0;
 
+function onAdd() {
+  const text = addbarInput.value;
+  if (text === "") {
+    addbarInput.focus();
+    return;
+  }
+
+  const newItem = createItem(text);
+  items.appendChild(newItem);
+  newItem.scrollIntoView();
+  addbarInput.value = "";
+  addbarInput.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
+
+  itemRow.innerHTML = `
+      <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete">
+           <i class="fas fa-trash-alt" data-id="${id}"></i>
+        </button>
+     </div>
+     <div class="item__divider"></div>
+  `;
+  id++;
+  return itemRow;
+}
 addbarBtn.addEventListener("click", (event) => {
   event.preventDefault();
+  onAdd();
+});
 
-  const addbarInputValue = addbarInput.value;
-  const newTask = document.createElement("li");
-  newTask.setAttribute("class", "item");
+items.addEventListener("click", (e) => {
+  const target = e.target;
+  const id = target.dataset.id;
+  if (id && target.nodeName === "I") {
+    const id = e.target.dataset.id;
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+    return;
+  }
 
-  const newTaskName = document.createElement("span");
-  newTaskName.textContent = addbarInputValue;
-
-  const newTaskDelete = document.createElement("i");
-  newTaskDelete.setAttribute("class", "fas fa-trash-alt");
-
-  newTaskDelete.addEventListener("click", (e) => {
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-  });
-
-  newTask.appendChild(newTaskName);
-  newTask.appendChild(newTaskDelete);
-
-  newTask.addEventListener("click", (e) => {
-    if (e.target.nodeName !== "SPAN") {
-      return;
-    }
+  if (target.nodeName === "SPAN") {
     if (e.target.style.textDecoration === "line-through") {
       e.target.style.textDecoration = "";
     } else {
       e.target.style.textDecoration = "line-through";
     }
-  });
-
-  content.appendChild(newTask);
-  addbarInput.value = "";
-  addbarInput.focus();
+    return;
+  }
 });
