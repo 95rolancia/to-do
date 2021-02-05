4e87ded3deadc20b498b0e82fb9424c7041ba2db
jangjunhyeok <95jjangjun@gmail.com>
2021-02-05 13:45:54 +0900

    Add saving the data before refresh

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 const items = document.querySelector(".items");
 const addbarBtn = document.querySelector(".addbar__btn");
 const addbarInput = document.querySelector(".addbar__input");
-let id = 0;
+const addbarForm = document.querySelector(".addbar__form");
 
-function onAdd() {
+const datas = "datas";
+let savedDatas = {};
+
+function onAdd(e) {
+  e.preventDefault();
   const text = addbarInput.value;
   if (text === "") {
     addbarInput.focus();
     return;
   }
-
-  const newItem = createItem(text);
-  items.appendChild(newItem);
+  savedDatas.id++;
+  savedDatas.list.push({ id: savedDatas.id, name: text });
+  save();
+  const newItem = createItem(text, savedDatas.id);
+  paintItem(newItem);
   newItem.scrollIntoView();
   addbarInput.value = "";
-  addbarInput.focus();
 }
 
-function createItem(text) {
+function paintItem(item) {
+  items.appendChild(item);
+}
+
+function createItem(text, id) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
   itemRow.setAttribute("data-id", id);
@@ -31,30 +40,63 @@ function createItem(text) {
      </div>
      <div class="item__divider"></div>
   `;
-  id++;
   return itemRow;
 }
-addbarBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  onAdd();
-});
 
 items.addEventListener("click", (e) => {
   const target = e.target;
   const id = target.dataset.id;
   if (id && target.nodeName === "I") {
-    const id = e.target.dataset.id;
+    savedDatas.list = savedDatas.list.filter((item) => item.id != id);
     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
     toBeDeleted.remove();
+    save();
     return;
   }
 
   if (target.nodeName === "SPAN") {
-    if (e.target.style.textDecoration === "line-through") {
-      e.target.style.textDecoration = "";
+    if (target.style.textDecoration === "line-through") {
+      target.style.textDecoration = "";
     } else {
-      e.target.style.textDecoration = "line-through";
+      target.style.textDecoration = "line-through";
     }
     return;
   }
 });
+
+function save() {
+  localStorage.setItem(datas, JSON.stringify(savedDatas));
+}
+
+function load() {
+  savedDatas = JSON.parse(localStorage.getItem(datas)) || {
+    id: 0,
+    list: [],
+  };
+  id = savedDatas.id;
+  console.log(savedDatas);
+  if (isEmptyObject(savedDatas)) {
+    console.log("저장된 데이터가 없습니다.");
+    return;
+  }
+
+  restore();
+}
+
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length && obj.constructor === Object;
+}
+
+function restore() {
+  savedDatas.list.forEach((savedItem) => {
+    const item = createItem(savedItem.name, savedItem.id);
+    paintItem(item);
+  });
+}
+
+function start() {
+  load();
+  addbarForm.addEventListener("submit", (e) => onAdd(e));
+}
+
+start();
